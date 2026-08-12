b75e6bfc3898b3c8d20a4bb07b4adf28e6270acd
ymgrmny-gif <ymgrmny@gmail.com>
2026-08-12 11:47:56 +0200

    Add robots policy

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/documents/', '/access/', '/api/'],
+      },
+    ],
+    sitemap: 'https://yalcinmutlu.pages.dev/sitemap.xml',
+    host: 'https://yalcinmutlu.pages.dev',
+  };
+}
