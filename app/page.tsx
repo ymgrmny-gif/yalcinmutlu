858e102555962233833cfd96013d9210e2b37623
ymgrmny-gif <ymgrmny@gmail.com>
2026-08-21 22:31:57 +0200

    Remove duplicate WhatsApp helper wiring

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import PortfolioPage from '@/components/PortfolioPage';
-import WhatsAppProfileLink from '@/components/WhatsAppProfileLink';
 
 export default function Home() {
-  return (
-    <>
-      <PortfolioPage />
-      <WhatsAppProfileLink />
-    </>
-  );
+  return <PortfolioPage />;
 }
